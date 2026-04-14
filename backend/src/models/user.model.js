6d0f4a17f89b5a9a967453ae898ddd92bf7454cc
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-14 11:16:30 +0500

    -created a user schema

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: [true, "Name must be unique"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Email must be unique"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  timestamps: true,
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
