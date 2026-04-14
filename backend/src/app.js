e554ea79b6f3205251a85347dfb4956152feb152
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-14 11:22:12 +0500

    -setup a authRoute

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const authRouter = require("./routes/authRoutes");
 
 const app = express();
 app.use(express.json());
+
+/**
+ * added the prefix "/api/auth" to all routes defined in the authRouter. This means that any route defined in authRouter will be accessible under the "/api/auth" path. For example, if authRouter has a route defined as "/login", it will be accessible at "/api/auth/login".
+ */
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the PerfectFit-AI backend!");
