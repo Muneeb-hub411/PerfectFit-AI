084cfb7312f2e27679f01032994849c5c23eadf7
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-15 21:41:35 +0500

    -created a login controller

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -2,6 +2,12 @@ const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+/**
+ * Register a new user
+ * @route POST /api/auth/register
+ *
+ *
+ */
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
@@ -29,6 +35,33 @@ const registerUser = async (req, res) => {
   res.cookie("token", token);
 
   res.status(201).json({ message: "User registered successfully", user });
+};
+
+/**
+ * Login a user
+ * @route POST /api/auth/login
+ * @desc Login a user and return a JWT token
+ * @access Public
+ */
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1D",
+  });
+  res.cookie("token", token);
+  res.status(200).json({ message: "User logged in successfully", user });
 };
 
 module.exports = { registerUser };
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,6 +3,11 @@ const authcontroller = require("../controllers/authControllers.js");
 
 const authRouter = Router();
 
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */
 authRouter.post("/register", authcontroller.registerUser);
 
 module.exports = authRouter;
