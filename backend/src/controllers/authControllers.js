54f2ab3ae05d43e8de4b85fa8e24e718c4c80c97
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-16 22:01:12 +0500

    -added a logout controller

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -64,4 +64,8 @@ const loginUser = async (req, res) => {
   res.status(200).json({ message: "User logged in successfully", user });
 };
 
-module.exports = { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ message: "User logged out successfully" });
+};
+module.exports = { registerUser, loginUser, logoutUser };
