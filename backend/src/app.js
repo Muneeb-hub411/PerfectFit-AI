c465d3788ebf16c3a9b898fbabc7ca331d180abb
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-17 11:44:08 +0500

    -setup morgan and cookies parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authRoutes");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("tiny"));
+
 app.use(express.json());
+app.use(cookieParser());
 
 /**
  * added the prefix "/api/auth" to all routes defined in the authRouter. This means that any route defined in authRouter will be accessible under the "/api/auth" path. For example, if authRouter has a route defined as "/login", it will be accessible at "/api/auth/login".
