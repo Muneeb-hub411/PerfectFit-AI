e80f947605cfcd5070f3f062cb0ee690e61ac255
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-20 17:41:18 +0500

    -created a signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "./pages/login.jsx";
+import Signup from "./pages/signup.jsx";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
