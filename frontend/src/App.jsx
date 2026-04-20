5d06aade40a67fd3633126c5560554f0cd22230d
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-20 17:29:52 +0500

    -Set up a react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Login from "./pages/login.jsx"
+import Login from "./pages/login.jsx";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <Login />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 };
