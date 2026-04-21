88fd27f35b23d6e23da92c59ad94077c358d54f4
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-21 21:39:19 +0500

    -setup react toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Login from "./pages/login.jsx";
 import Signup from "./pages/signup.jsx";
 import { Routes, Route } from "react-router-dom";
-
+import { AuthProvider } from "./context/authContext.jsx";
+import { ToastContainer } from "react-toastify";
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </>
+      <ToastContainer />
+    </AuthProvider>
   );
 };
 
