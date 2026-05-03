e35f2c157d5d4e80006b96995ce1f2374e1aaa33
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-05-03 10:04:01 +0500

    -added a middleware

--- backend/src/routes/interviewreportRoute.js
+++ backend/src/routes/interviewreportRoute.js
@@ -1,6 +1,8 @@
 const reportController = require("../controllers/reportController");
 
 const { Router } = require("express");
+const { authMiddleware } = require("../middleware/auth.middleware");
+const upload = require("../config/confi.multer");
 
 const interviewReportRouter = Router();
 
@@ -9,6 +11,11 @@ const interviewReportRouter = Router();
  * @desc Generate an interview report based on the provided resume, self-description, job title, and job description.
  * @access Private
  ********************/
-interviewReportRouter.post("/generate", reportController.generateReport);
+interviewReportRouter.post(
+  "/generate",
+  authMiddleware,
+  upload.single("resume"),
+  reportController.generateReport,
+);
 
 module.exports = interviewReportRouter;
