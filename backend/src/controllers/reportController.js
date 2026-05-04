bb0491a18af72a1843403eb91fc379432a8e7319
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-05-04 21:24:23 +0500

    added a getreport controller

--- backend/src/controllers/reportController.js
+++ backend/src/controllers/reportController.js
@@ -4,19 +4,16 @@ const PDFParser = require("pdf-parse");
 
 const generateReport = async (req, res) => {
   try {
-    // PDF check karo
     if (!req.file) {
       return res.status(400).json({ message: "Resume PDF is required" });
     }
 
     const { self_description, job_title, job_description } = req.body;
 
-    // Fields check karo
     if (!self_description || !job_title || !job_description) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    // PDF se text nikalo
     const pdfData = await PDFParser(req.file.buffer);
     const resume_text = pdfData.text;
 
@@ -54,6 +51,17 @@ const generateReport = async (req, res) => {
   }
 };
 
+const getreportByUserId = async (req, res) => {
+  try {
+    const reports = await interviewReportModel.find({ user_id: req.userId });
+    res.status(200).json({ reports });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   generateReport,
+  getreportByUserId,
 };
